fd015df013bae5b1e235ed451e3ce16f24b984ce
Arti <artem.kort@stiftsgymnasium.de>
2026-09-19 13:13:48 +0200

    Add helper method for enchant register

--- src/main/java/com/endchants/ModEnchantments.java
+++ src/main/java/com/endchants/ModEnchantments.java
@@ -1,11 +1,14 @@
 package com.endchants;
 
+import java.util.List;
+
 import com.endchants.enchantment.effect.BuffOnKillEffect;
 import com.endchants.enchantment.effect.FleshEaterEffect;
 import com.endchants.enchantment.effect.RadianceEffect;
 import com.endchants.enchantment.effect.RepellingEffect;
 import com.endchants.enchantment.subpredicate.IsHurtPredicate;
 import net.minecraft.advancements.criterion.EntityPredicate;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
@@ -17,7 +20,9 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
 import net.minecraft.world.item.enchantment.EnchantmentTarget;
 import net.minecraft.world.item.enchantment.LevelBasedValue;
+import net.minecraft.world.item.enchantment.TargetedConditionalEffect;
 import net.minecraft.world.item.enchantment.effects.AddValue;
+import net.minecraft.world.item.enchantment.effects.EnchantmentEntityEffect;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
 
@@ -35,6 +40,38 @@ public class ModEnchantments {
                 return ResourceKey.create(Registries.ENCHANTMENT, Endchants.id(name));
         }
 
+        public static Enchantment.Builder weaponEnchant(BootstrapContext<Enchantment> ctx, int weight, int levels,
+                        Enchantment.Cost min, Enchantment.Cost max, int anvilCost, EnchantmentEntityEffect effect) {
+                return weaponEnchant(ctx, weight, levels, min, max, anvilCost)
+                                .withEffect(EnchantmentEffectComponents.POST_ATTACK, EnchantmentTarget.ATTACKER,
+                                                EnchantmentTarget.VICTIM, effect);
+        }
+
+        public static Enchantment.Builder weaponEnchant(BootstrapContext<Enchantment> ctx, int weight, int levels,
+                        Enchantment.Cost min, Enchantment.Cost max, int anvilCost) {
+                return Enchantment
+                                .enchantment(Enchantment.definition(
+                                                ctx.lookup(Registries.ITEM).getOrThrow(ItemTags.WEAPON_ENCHANTABLE),
+                                                weight, levels, min, max, anvilCost, EquipmentSlotGroup.HAND));
+        }
+
+        public static Enchantment.Builder armorEnchant(BootstrapContext<Enchantment> ctx, int weight, int levels,
+                        Enchantment.Cost min, Enchantment.Cost max, int anvilCost) {
+                return Enchantment
+                                .enchantment(Enchantment.definition(
+                                                ctx.lookup(Registries.ITEM).getOrThrow(ItemTags.ARMOR_ENCHANTABLE),
+                                                weight, levels, min, max, anvilCost, EquipmentSlotGroup.ARMOR));
+        }
+
+        public static <T extends EnchantmentEntityEffect> Enchantment.Builder armorEnchant(
+                        BootstrapContext<Enchantment> ctx, int weight, int levels,
+                        Enchantment.Cost min, Enchantment.Cost max, int anvilCost,
+                        DataComponentType<List<TargetedConditionalEffect<T>>> when,
+                        EnchantmentTarget affected, T effect) {
+                return armorEnchant(ctx, weight, levels, min, max, anvilCost).withEffect(when, EnchantmentTarget.VICTIM,
+                                affected, effect);
+        }
+
         public static ResourceKey<Enchantment> FLESH_EATER = genKey("flesh_eater");
         public static ResourceKey<Enchantment> REPELLING = genKey("repelling");
         public static ResourceKey<Enchantment> RADIANCE = genKey("radiance");
@@ -44,115 +81,36 @@ public class ModEnchantments {
 
         public static void init(BootstrapContext<Enchantment> ctx) {
                 register(ctx, FLESH_EATER,
-                                Enchantment.enchantment(
-                                                Enchantment.definition(
-                                                                ctx.lookup(Registries.ITEM)
-                                                                                .getOrThrow(ItemTags.WEAPON_ENCHANTABLE),
-                                                                10,
-                                                                3,
-                                                                Enchantment.dynamicCost(6, 8),
-                                                                Enchantment.dynamicCost(8, 8),
-                                                                5,
-                                                                EquipmentSlotGroup.HAND))
-                                                .withEffect(
-                                                                EnchantmentEffectComponents.POST_ATTACK,
-                                                                EnchantmentTarget.ATTACKER,
-                                                                EnchantmentTarget.VICTIM,
-                                                                new FleshEaterEffect(LevelBasedValue.perLevel(1, 2))));
+                                weaponEnchant(ctx, 10, 3, Enchantment.dynamicCost(6, 8), Enchantment.dynamicCost(8, 8),
+                                                5, new FleshEaterEffect(LevelBasedValue.perLevel(1, 2))));
                 register(ctx, REPELLING,
-                                Enchantment.enchantment(
-                                                Enchantment.definition(
-                                                                ctx.lookup(Registries.ITEM)
-                                                                                .getOrThrow(ItemTags.ARMOR_ENCHANTABLE),
-                                                                10,
-                                                                2,
-                                                                Enchantment.dynamicCost(6, 8),
-                                                                Enchantment.dynamicCost(8, 8),
-                                                                8,
-                                                                EquipmentSlotGroup.ARMOR))
-                                                .withEffect(
-                                                                EnchantmentEffectComponents.POST_ATTACK,
-                                                                EnchantmentTarget.VICTIM,
-                                                                EnchantmentTarget.ATTACKER,
-                                                                new RepellingEffect(0.2f)));
-                register(ctx, RADIANCE, Enchantment.enchantment(
-                                Enchantment.definition(
-                                                ctx.lookup(Registries.ITEM)
-                                                                .getOrThrow(ItemTags.WEAPON_ENCHANTABLE),
-                                                6,
-                                                2,
-                                                Enchantment.dynamicCost(8, 8),
-                                                Enchantment.dynamicCost(28,
-                                                                8),
-                                                8,
-                                                EquipmentSlotGroup.HAND))
-                                .withEffect(
-                                                EnchantmentEffectComponents.POST_ATTACK,
-                                                EnchantmentTarget.ATTACKER,
-                                                EnchantmentTarget.VICTIM,
-                                                new RadianceEffect(1)));
-                register(ctx, COMMITTED, Enchantment.enchantment(
-                                Enchantment.definition(
-                                                ctx.lookup(Registries.ITEM)
-                                                                .getOrThrow(ItemTags.WEAPON_ENCHANTABLE),
-                                                6,
-                                                2,
-                                                Enchantment.dynamicCost(8, 8),
-                                                Enchantment.dynamicCost(28,
-                                                                8),
-                                                8,
-                                                EquipmentSlotGroup.HAND))
+                                armorEnchant(ctx, 10, 2, Enchantment.dynamicCost(6, 8), Enchantment.dynamicCost(8, 8),
+                                                8, EnchantmentEffectComponents.POST_ATTACK, EnchantmentTarget.ATTACKER,
+                                                new RepellingEffect(.2f)));
+                register(ctx, RADIANCE,
+                                weaponEnchant(ctx, 6, 2, Enchantment.dynamicCost(8, 8), Enchantment.dynamicCost(28,
+                                                8), 8, new RadianceEffect(1)));
+                register(ctx, COMMITTED, weaponEnchant(ctx, 6, 2, Enchantment.dynamicCost(8, 8),
+                                Enchantment.dynamicCost(28, 8), 8)
                                 .withEffect(
                                                 EnchantmentEffectComponents.DAMAGE,
                                                 new AddValue(LevelBasedValue.perLevel(3.0F)),
                                                 LootItemEntityPropertyCondition.hasProperties(
-                                                                LootContext.EntityTarget.THIS, // "THIS" refers to the
-                                                                                               // target/victim in
-                                                                                               // DAMAGE context
+                                                                LootContext.EntityTarget.THIS,
                                                                 EntityPredicate.Builder.entity()
-                                                                                // Check that health is less than max
-                                                                                // health
-                                                                                .subPredicate(new IsHurtPredicate()) // or
-                                                                                                                     // custom
-                                                                                                                     // predicate
-                                                )));
-                register(ctx, GUARDING_STRIKE, Enchantment.enchantment(
-                                Enchantment.definition(
-                                                ctx.lookup(Registries.ITEM)
-                                                                .getOrThrow(ItemTags.WEAPON_ENCHANTABLE),
-                                                6,
-                                                3,
-                                                Enchantment.dynamicCost(8, 8),
-                                                Enchantment.dynamicCost(28,
-                                                                8),
-                                                8,
-                                                EquipmentSlotGroup.HAND))
-                                .withEffect(
-                                                EnchantmentEffectComponents.POST_ATTACK,
-                                                EnchantmentTarget.ATTACKER,
-                                                EnchantmentTarget.VICTIM,
-                                                new BuffOnKillEffect(LevelBasedValue.perLevel(2, 0),
+
+                                                                                .subPredicate(new IsHurtPredicate()))));
+                register(ctx, GUARDING_STRIKE,
+                                weaponEnchant(ctx, 6, 3, Enchantment.dynamicCost(8, 8), Enchantment.dynamicCost(28, 8),
+                                                8, new BuffOnKillEffect(LevelBasedValue.perLevel(2, 0),
                                                                 LevelBasedValue.perLevel(2),
                                                                 BuiltInRegistries.MOB_EFFECT.get(Identifier
                                                                                 .withDefaultNamespace("resistance"))
                                                                                 .get(),
                                                                 .2f)));
-                register(ctx, RAMPAGING, Enchantment.enchantment(
-                                Enchantment.definition(
-                                                ctx.lookup(Registries.ITEM)
-                                                                .getOrThrow(ItemTags.WEAPON_ENCHANTABLE),
-                                                6,
-                                                3,
-                                                Enchantment.dynamicCost(8, 8),
-                                                Enchantment.dynamicCost(28,
-                                                                8),
-                                                8,
-                                                EquipmentSlotGroup.HAND))
-                                .withEffect(
-                                                EnchantmentEffectComponents.POST_ATTACK,
-                                                EnchantmentTarget.ATTACKER,
-                                                EnchantmentTarget.VICTIM,
-                                                new BuffOnKillEffect(LevelBasedValue.perLevel(0, 0),
+                register(ctx, RAMPAGING,
+                                weaponEnchant(ctx, 6, 3, Enchantment.dynamicCost(8, 8), Enchantment.dynamicCost(28, 8),
+                                                8, new BuffOnKillEffect(LevelBasedValue.perLevel(0, 0),
                                                                 LevelBasedValue.perLevel(5),
                                                                 ModEffects.RAMPAGING,
                                                                 .1f)));
